ad8e2eaf95e1a9ae0f53cd5bc0b50c07a860793f
Leonardo Sarmento de Castro <leonardo.sarmentocastro@gmail.com>
2017-11-15 23:27:12 -0200

    FullHeightCenteredForm component - Refactor
    Details
    . Broken into small function components to improve readability.

--- src/app/modules/shared/components/full-height-centered-form.component.js
+++ src/app/modules/shared/components/full-height-centered-form.component.js
@@ -1,7 +1,7 @@
 /**
  * NPM packages.
  */
-import React, { Component } from 'react';
+import React from 'react';
 import {
   BrowserRouter as Router,
   Route
@@ -10,39 +10,53 @@ import {
 /**
  * Project packages.
  */
-import SignUpComponent from './../../sign-up/components/sign-up.component';
+import SignUp from './../../sign-up/components/sign-up.component';
 
-class FullHeightCenteredForm extends Component {
-  render() {
-    const template = (
-      <section className='section is-flex full-height valign-center'>
-        <div className="container">
-          <div className="columns">
-            {/* content */}
-            <div className="column is-centered">
-              <div className="content is-medium has-text-centered">
-                <h1 className='title'>Welcome to <span className='-is-affinitas is-primary'>Affinitas</span></h1>
-              </div>
-
-              <div className="columns">
-                <div className="column is-one-third"></div>
-                <div className="column is-one-third">
-                  <Router>
-                    <Route exact path='/sign_up' component={SignUpComponent} />
-                    { /* TODO:  <Route exact path='/sign_in' component={} /> */}
-                  </Router>
-                </div>
-                <div className="column is-one-third"></div>
-              </div>
-            </div>
-            {/* content */}
-          </div>
+/**
+ * Building the "FullHeightCenteredForm".
+ */
+const FullHeightContainer = (props) => (
+  <section className='section is-flex full-height valign-center'>
+    <div className="container">
+      <div className="columns">
+        <div className="column is-centered">
+          { props.children }
         </div>
-      </section>
-    );
+      </div>
+    </div>
+  </section>
+);
 
-    return template;
-  }
-}
+const WelcomeTitle = () => (
+  <div className="content is-medium has-text-centered">
+    <h1 className='title'>Welcome to <span className='-is-affinitas is-primary'>Affinitas</span></h1>
+  </div>
+);
+
+const CenteredForm = (props) => (
+  <div className="columns">
+    <div className="column is-one-third"></div>
+    <div className="column is-one-third">
+      { props.children }
+    </div>
+    <div className="column is-one-third"></div>
+  </div>
+);
+
+const Content = () => (
+  <Router>
+    <Route exact path='/sign_up' component={SignUp} />
+  </Router>
+);
+
+const FullHeightCenteredForm = () => (
+  <FullHeightContainer>
+    <WelcomeTitle />
+
+    <CenteredForm>
+      <Content />
+    </CenteredForm>
+  </FullHeightContainer>
+);
 
 export default FullHeightCenteredForm;
